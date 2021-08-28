c3174c3e5a498a902c2656c7f817dc3fe713e276
C-Brooker <brookercharlie2003@gmail.com>
2021-08-28 20:33:30 +0100

    fix(pagination): fixed pagination

--- src/components/pages/BlogDisplay.js
+++ src/components/pages/BlogDisplay.js
@@ -10,6 +10,10 @@ import { whichPage, numbersOnButtons } from "../utility/pagination.js";
 function BlogDisplay() {
   //useState hook sets the initial state in the component as an empty array
   const [blogPosts, setBlogPosts] = useState([]);
+  const blogList = Object.values(blogPosts).reverse();
+  const initial = blogList.slice(0, 4);
+  console.log("bloglisttt: ", blogList);
+  console.log("initialllll: ", initial);
   //set which page will display for blog posts
   const [showPosts, setShowPosts] = useState([]);
   //function to perform fetch request
@@ -24,6 +28,7 @@ function BlogDisplay() {
 
     //update the state of the component with the new database data
     setBlogPosts(data);
+    setShowPosts(blogList);
   }
   //with an empty dependency array, every time the page renders,
   //the get request (fetch) is called and the state of the BlogDisplay component
@@ -32,39 +37,7 @@ function BlogDisplay() {
     dataExtractor();
   }, []);
 
-  const initialArray = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-
-  const blogList = Object.values(blogPosts).reverse();
-
-  const buttons = numbersOnButtons(blogList, 2);
-  console.log(buttons);
+  const buttons = numbersOnButtons(blogList, 3);
 
   const clickHandler = (e) => {
     // const intId = parseInt(id);
@@ -73,12 +46,17 @@ function BlogDisplay() {
     setShowPosts(postsToShow);
   };
 
+  // const displayPosts = showPosts ? showPosts : blogList.slice(0, 4);
+  console.log(showPosts);
+  // console.log(initialPosts);
   return (
     <div>
       <TitleBanner>blogDisplay</TitleBanner>
       <hr />
       <AboutMeNav />
-      <BlogPostList blogposts={showPosts} />
+      <hr />
+      <BlogPostList blogposts={showPosts == ![] ? initial : showPosts} />
+      <hr />
       <div>
         <ul>
           {buttons.slice(1).map((button) => {
@@ -95,6 +73,7 @@ function BlogDisplay() {
           })}
         </ul>
       </div>
+      <hr />
       <Footer />
     </div>
   );
